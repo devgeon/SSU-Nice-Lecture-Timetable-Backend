d254f04418dbbc7656cc9fb77cc5121883712022
devgeon <devgeon.git@gmail.com>
2021-11-13 11:45:53 +0900

    Add subject json upload page

--- subject/index.js
+++ subject/index.js
@@ -3,6 +3,7 @@ const router = Router();
 const ctrl = require('./subject.ctrl');
 
 // get
+router.get('/timetable', ctrl.page)
 router.post('/timetable', ctrl.data);
 
 module.exports = router;
--- subject/subject.ctrl.js
+++ subject/subject.ctrl.js
@@ -2,10 +2,16 @@ const { db } = require('../constants/constants');
 
 const data = async function(req, res) {
     let data = req.body;
+    try {
+        data = data.json;
+        data = JSON.parse(data);
+    }
+    catch(err) { console.log(err) }
     let connection = await db.getConnection();
     // console.log(data);
-    for (i=0;i<201;i++) {
-        let { course_number, regexp } = data; 
+    let { course_number, regexp } = data;
+    // console.log(course_number);
+    for (i=1400;i<1408;i++) {
         console.log(course_number[i]);
         console.log(regexp[i]);
         regexp[i] = JSON.stringify(regexp[i]);
@@ -20,6 +26,15 @@ const data = async function(req, res) {
     return res.status(200).send('OK');
 }
 
+const page = (req, res) => {
+    let body = `
+    <form id='form' action='/subject/timetable' method='post'>
+        <input type='text' name='json' id='json' maxLength='2500'>
+        <input type='submit'>
+    `;
+    res.send(body);
+}
+
 module.exports = {
-    data,
+    data, page
 }
